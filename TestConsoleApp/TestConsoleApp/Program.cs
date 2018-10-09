e207adcb5ae036a7d61e448a41c13541507a768b
Kirk Evans <kirke@risa.com>
2018-10-09 16:16:10 -0700

    ;lkj;l

--- TestConsoleApp/TestConsoleApp/Program.cs
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world-aft-reins12");
+            Console.WriteLine("Hello world-aft-reins13");
             var buf = Console.Read();
         }
     }
